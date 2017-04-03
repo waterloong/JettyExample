5b7f580be3dda4d2404e352e5ce6e7f8a7119ee1
William Zhang <will800228@gmail.com>
2017-04-02 22:55:05 -0400

    add servlet

--- src/main/java/OneHandler.java
+++ src/main/java/OneHandler.java
@@ -1,4 +1,6 @@
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class OneHandler
 {
@@ -6,7 +8,13 @@ public class OneHandler
     {
         Server server = new Server(8080);
 //        server.setHandler(new HelloHandler());
-        server.setHandler(new PythagoreanHandler());
+//        server.setHandler(new PythagoreanHandler());
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(new ExampleServlet()),"/search");
+
         server.start();
         server.join();
     }
